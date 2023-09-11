018a12196b86642c8b19c02de8484d5624670308
Myrz007 <122377787+Myrz007@users.noreply.github.com>
2023-09-11 11:32:43 +0200

    Add some selector variables

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const form = document.querySelector('form');
+const firstName = document.getElementById('fname');
+const fNameError = document.querySelector('#fname + span.error');
+const lastName = document.getElementById('lname');
+const lNameError = document.querySelector('#lname + span.error');
+const email = document.getElementById('email');
+const emailError = document.querySelector('#email + span.error');
+const password = document.getElementById('pwd');
+const pwdError = document.querySelector('#pwd + span.error');
+const confirmPwd = document.getElementById('confirm-pwd');
