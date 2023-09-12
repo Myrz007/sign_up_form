7a73fa6c763e01e08878acdce34af2cff3cd5263
Myrz007 <122377787+Myrz007@users.noreply.github.com>
2023-09-12 17:23:31 +0200

    Add some error handler functions

--- js/main.js
+++ js/main.js
@@ -9,62 +9,6 @@ const password = document.getElementById('pwd');
 const pwdError = document.querySelector('#pwd + span.error');
 const confirmPwd = document.getElementById('confirm-pwd');
 
-console.log(firstName)
-console.log(lastName)
-console.log(email)
-console.log(password)
-
-firstName.addEventListener('input', () => {
-    if (firstName.validity.valid) {
-        fNameError.textContent = '';
-        fNameError.className = 'error';
-    }
-    else fNameErrorHandler();
-});
-
-lastName.addEventListener('input', () => {
-    if (lastName.validity.valid) {
-        lNameError.textContent = '';
-        lNameError.className = 'error';
-    }
-    else lNameErrorHandler();
-});
-
-email.addEventListener('input', () => {
-    if (email.validity.valid) {
-        emailError.textContent = '';
-        emailError.className = 'error';
-    }
-    else emailErrorHandler();
-});
-
-password.addEventListener('input', () => {
-    if (password.validity.valid) {
-        pwdError.textContent = '';
-        pwdError.className = 'error';
-    }
-    else pwdErrorHandler();
-});
-
-form.addEventListener('submit', (event) => {
-    switch (true) {
-        case !firstName.validity.valid:
-            fNameErrorHandler();
-            event.preventDefault();
-        case !lastName.validity.valid:
-            lNameErrorHandler();
-            event.preventDefault();
-        case !email.validity.valid:
-            emailErrorHandler();
-            event.preventDefault();
-        case !password.validity.valid:
-            pwdErrorHandler();
-            event.preventDefault();
-        default:
-            return;
-    }
-});
-
 function fNameErrorHandler() {
     if (firstName.validity.valueMissing) fNameError.textContent = 'You need to enter your first name.';
     
